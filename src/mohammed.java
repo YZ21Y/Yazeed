78c429814a595859cc7d570cfeafe9e23fbad3a0
code_ <mhmd.abomkh4@gmail.com>
2026-06-02 20:29:12 +0300

    fro mohammed

--- src/mohammed.java
+++ src/mohammed.java
@@ -1,5 +1,5 @@
 public class mohammed {
     public static void main(String[] args) {
-        System.out.println("just mohammed can work on this class");
+        System.out.println("just mohammed can work on this java class");
     }
 }
