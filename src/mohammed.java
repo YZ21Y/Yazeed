a177e95385e88199d05b99f6bf6d0b736b56b0c9
l7dcodder <moraaalosh@gmail.com>
2026-06-02 20:40:09 +0300

    gg

--- src/mohammed.java
+++ src/mohammed.java
@@ -1,5 +1,5 @@
 public class mohammed {
     public static void main(String[] args) {
-        System.out.println("just mohammed can work on this java1 class");
+        System.out.println("no one care i changed it (ali) ");
     }
 }
