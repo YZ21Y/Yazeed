f8e9ad56bb20dde14c2bde84a8d1e5c4434ea31a
l7dcodder <moraaalosh@gmail.com>
2026-06-02 20:44:23 +0300

    gg

--- src/mohammed.java
+++ src/mohammed.java
@@ -1,5 +1,5 @@
 public class mohammed {
     public static void main(String[] args) {
-        System.out.println("no one care i changed it (alimorar) ");
+        System.out.println("no one care i changed it (alimorar1) ");
     }
 }
